4cbf837824dc7c7ba8024d79dabd33d3a08702c5
Kenneth Fabo <fabokenneth@gmail.com>
2021-04-21 16:41:50 +0100

    feature: add graphql fecth

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -41,9 +41,9 @@
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
-import { useI18n } from "vue-i18n";
-export default defineComponent({
+  import { ref, defineComponent,watch } from 'vue'
+  import { useI18n } from 'vue-i18n'
+  export default defineComponent({
   name: 'HelloWorld',
   props: {
     msg: {
@@ -52,11 +52,46 @@ export default defineComponent({
     }
   },
   setup: () => {
+
+    fetch('https://rickandmortyapi.com/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query:
+          `query {
+  characters(page: 1, filter: { name: "rick" }) {
+    info {
+      count
+    }
+    results {
+      id,
+      name,
+      species,
+      gender,
+      image
+    }
+  }
+  location(id: 1) {
+    id
+  }
+  episodesByIds(ids: [1, 2]) {
+    id
+  }
+}`
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.data)
+      });
+
     const count = ref(0)
     const i18n = useI18n({});
     return { t: i18n.t, count }
   }
-})
+  })
 </script>
 
 <style scoped>

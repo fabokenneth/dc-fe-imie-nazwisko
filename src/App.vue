ffe93e739f7bdb82dda28e017b16cd4e4b1e3ab9
Kenneth Fabo <fabokenneth@gmail.com>
2021-04-22 18:31:32 +0100

    FEATURE: dropdown menu ready

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div class="bg-red-900">
     <img alt="Vue logo" src="./assets/logo.png" />
   </div>
+  <dropdown-menu/>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue'
   import HelloWorld from './components/HelloWorld.vue'
+  import DropdownMenu from './components/DropdownMenu.vue'
 
   export default defineComponent({
     name: 'App',
     components: {
       HelloWorld,
+      "dropdown-menu": DropdownMenu
     },
   })
 </script>

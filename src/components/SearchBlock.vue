5ffcb7e97e1850f4ca095a882b1bc901ff4f861b
Kenneth Fabo <fabokenneth@gmail.com>
2021-04-24 15:10:46 +0100

    fix: import Proptype to be able to use it

--- src/components/SearchBlock.vue
+++ src/components/SearchBlock.vue
@@ -1,73 +1,79 @@
 <template>
-  <div class="flex items-center justify-start rounded-md border border-colliersGray-400 w-96 h-14">
+  <div
+    class="flex items-center justify-start rounded-md border border-colliersGray-400 w-96 h-14"
+  >
     <div class="flex items-center h-full">
       <div class="searchBoxText justify-center w-24">
-        {{ t("searchBy_lbl")}}
+        {{ t('searchBy_lbl') }}
       </div>
     </div>
     <div class="w-40 border-l border-r border-colliersGray-400 h-full">
-      <search-type-picker v-model="state.searchType"/>
+      <search-type-picker v-model="state.searchType" />
     </div>
     <div class="w-3/5">
-      <search-text-input v-model="state.searchText"/>
+      <search-text-input v-model="state.searchText" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent, reactive, watch} from "vue";
-import SearchTypePicker from "./SearchTypePicker.vue";
-import SearchTextInput from "./SearchTextInput.vue";
-import {SearchBy} from "../types/Ui.interface";
-import {useI18n} from "vue-i18n";
+  import { defineComponent, reactive, watch, PropType } from 'vue'
+  import SearchTypePicker from './SearchTypePicker.vue'
+  import SearchTextInput from './SearchTextInput.vue'
+  import { SearchBy } from '../types/Ui.interface'
+  import { useI18n } from 'vue-i18n'
 
-export default defineComponent({
-  name: "SearchBlock",
-  components: {
-    "search-type-picker": SearchTypePicker,
-    "search-text-input": SearchTextInput
-  },
-  props: {
-    searchText: {
-      type: String,
-      required: true,
+  export default defineComponent({
+    name: 'SearchBlock',
+    components: {
+      'search-type-picker': SearchTypePicker,
+      'search-text-input': SearchTextInput,
     },
-    searchType: {
-      type: String as PropType<SearchBy>,
-      required: true
-    }
-  },
-  emits: ["update:searchText", "update:searchType"],
-  setup(props, {emit}) {
-    const i18n = useI18n({
-      messages: {
-        en: {
-          searchBy_lbl: "Search By"
+    props: {
+      searchText: {
+        type: String,
+        required: true,
+      },
+      searchType: {
+        type: String as PropType<SearchBy>,
+        required: true,
+      },
+    },
+    emits: ['update:searchText', 'update:searchType'],
+    setup(props, { emit }) {
+      const i18n = useI18n({
+        messages: {
+          en: {
+            searchBy_lbl: 'Search By',
+          },
+          fr: {
+            searchBy_lbl: 'Rechercher Par',
+          },
+          de: {
+            searchBy_lbl: 'Suchen nach',
+          },
         },
-        fr: {
-          searchBy_lbl: "Rechercher Par"
-        },
-        de: {
-          searchBy_lbl: "Suchen nach"
-        }
+      })
+      const state = reactive({
+        searchType: props.searchType,
+        searchText: props.searchText,
+      })
+
+      watch(
+        () => state.searchText,
+        (newValue) => emit('update:searchText', newValue)
+      )
+      watch(
+        () => state.searchType,
+        (newValue) => emit('update:searchType', newValue)
+      )
+
+      return {
+        t: i18n.t,
+        state,
       }
-    })
-    const state = reactive({
-      searchType: props.searchType,
-      searchText: props.searchText,
-    });
-
-    watch(() => state.searchText, (newValue) => emit("update:searchText", newValue));
-    watch(() => state.searchType, (newValue) => emit("update:searchType", newValue));
-
-    return {
-      t: i18n.t,
-      state
-    }
-  }
-})
+    },
+  })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

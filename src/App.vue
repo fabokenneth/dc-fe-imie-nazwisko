f71d4c4423c91caedaf36371c49bcb284b803025
Kenneth Fabo <fabokenneth@gmail.com>
2021-04-25 18:34:45 +0100

    FIX: selected tab only should have border on bottom

--- src/App.vue
+++ src/App.vue
@@ -114,7 +114,14 @@
 </template>
 
 <script lang="ts">
-import {computed, defineComponent, nextTick, onMounted, reactive, watch} from 'vue'
+  import {
+    computed,
+    defineComponent,
+    nextTick,
+    onMounted,
+    reactive,
+    watch,
+  } from 'vue'
   import { SearchBy } from './types/Ui.interface'
   import { Character } from './types/CharactersType.interface'
   import LanguagePicker from './components/LanguagePicker.vue'
@@ -122,239 +129,254 @@ import {computed, defineComponent, nextTick, onMounted, reactive, watch} from 'v
   import Pagination from './components/Pagination.vue'
   import Gender from './components/Gender.vue'
   import { useI18n } from 'vue-i18n'
-import {fetchByIds, getCharacters} from './services/RichAndMortyAPI'
+  import { fetchByIds, getCharacters } from './services/RichAndMortyAPI'
 
   enum TabType {
     AllCharacters,
     Favorites,
   }
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    "language-picker": LanguagePicker,
-    "search-block": SearchBlock,
-    "pagination": Pagination,
-    "gender": Gender
-  },
-  setup() {
-    const i18n = useI18n({
-      messages: {
-        en: {
-          datagrid: {
-            allCharacters_btn: "All Characters",
-            favorites_btn: "Favorites",
-            photo_th: "Photo",
-            characterId_th: "Character ID",
-            name_th: "Name",
-            gender_th: "Gender",
-            species_th: "Species",
-            lastEpisode_th: "Last Episode",
-            addToFavorites_th: "Add To Favorites",
+  export default defineComponent({
+    name: 'App',
+    components: {
+      'language-picker': LanguagePicker,
+      'search-block': SearchBlock,
+      pagination: Pagination,
+      gender: Gender,
+    },
+    setup() {
+      const i18n = useI18n({
+        messages: {
+          en: {
+            datagrid: {
+              allCharacters_btn: 'All Characters',
+              favorites_btn: 'Favorites',
+              photo_th: 'Photo',
+              characterId_th: 'Character ID',
+              name_th: 'Name',
+              gender_th: 'Gender',
+              species_th: 'Species',
+              lastEpisode_th: 'Last Episode',
+              addToFavorites_th: 'Add To Favorites',
+            },
+            gender: {
+              Male: 'Male',
+              Female: 'Female',
+              Genderless: 'Genderless',
+              unknown: 'Unknown',
+            },
+            species: {
+              Human: 'Human',
+              Alien: 'Alien',
+              unknown: 'Unknown',
+              Humanoid: 'Humanoid',
+              Poopybutthole: 'Poopybutthole',
+              Animal: 'Animal',
+              MythologicalCreature: 'Mythological Creature',
+              Robot: 'Robot',
+              Cronenberg: 'Cronenberg',
+              Disease: 'Disease',
+            },
           },
-          gender: {
-            Male: "Male",
-            Female: "Female",
-            Genderless: "Genderless",
-            unknown: "Unknown"
+          fr: {
+            datagrid: {
+              allCharacters_btn: 'Tous Les Personnages',
+              favorites_btn: 'Favoris',
+              photo_th: 'Photo',
+              characterId_th: 'ID du Personnage',
+              name_th: 'Nom',
+              gender_th: 'Genre',
+              species_th: 'Espèces',
+              lastEpisode_th: 'Dernier Épisode',
+              addToFavorites_th: 'Ajouter Aux Favoris',
+            },
+            gender: {
+              Male: 'Mâle',
+              Female: 'Femelle',
+              Genderless: 'Sans sexe',
+              unknown: 'Inconnu',
+            },
+            species: {
+              Human: 'Humain',
+              Alien: 'Extraterrestre',
+              unknown: 'Inconnu',
+              Humanoid: 'Humanoide',
+              Poopybutthole: 'Trou de cul de poopy',
+              Animal: 'Animal',
+              MythologicalCreature: 'Créature mythologique',
+              Robot: 'Robot',
+              Cronenberg: 'Cronenberg',
+              Disease: 'Maladie',
+            },
           },
-          species: {
-            Human: "Human",
-            Alien: "Alien",
-            unknown: "Unknown",
-            Humanoid: "Humanoid",
-            Poopybutthole: "Poopybutthole",
-            Animal: "Animal",
-            MythologicalCreature: "Mythological Creature",
-            Robot: "Robot",
-            Cronenberg: "Cronenberg",
-            Disease: "Disease"
-          }
+          de: {
+            datagrid: {
+              allCharacters_btn: 'Alle Charaktere',
+              favorites_btn: 'Favoriten',
+              photo_th: 'Foto',
+              characterId_th: 'Charakter-ID',
+              name_th: 'Name',
+              gender_th: 'Geschlecht',
+              species_th: 'Spezies',
+              lastEpisode_th: 'Letzte Folge',
+              addToFavorites_th: 'Zu den Favoriten',
+            },
+            gender: {
+              Male: 'Männlich',
+              Female: 'Weiblich',
+              Genderless: 'Geschlechtslos',
+              unknown: 'Unbekannt',
+            },
+            species: {
+              Human: 'Mensch',
+              Alien: 'Außerirdischer',
+              unknown: 'Unbekannt',
+              Humanoid: 'Humanoid',
+              Poopybutthole: 'Poopybutthole',
+              Animal: 'Tier',
+              MythologicalCreature: 'Mythologische Kreatur',
+              Robot: 'Roboter',
+              Cronenberg: 'Cronenberg',
+              Disease: 'Krankheit',
+            },
+          },
         },
-        fr: {
-          datagrid: {
-            allCharacters_btn: "Tous Les Personnages",
-            favorites_btn: "Favoris",
-            photo_th: "Photo",
-            characterId_th: "ID du Personnage",
-            name_th: "Nom",
-            gender_th: "Genre",
-            species_th: "Espèces",
-            lastEpisode_th: "Dernier Épisode",
-            addToFavorites_th: "Ajouter Aux Favoris",
-          },
-          gender: {
-            Male: "Mâle",
-            Female: "Femelle",
-            Genderless: "Sans sexe",
-            unknown: "Inconnu"
-          },
-          species: {
-            Human: "Humain",
-            Alien: "Extraterrestre",
-            unknown: "Inconnu",
-            Humanoid: "Humanoide",
-            Poopybutthole: "Trou de cul de poopy",
-            Animal: "Animal",
-            MythologicalCreature: "Créature mythologique",
-            Robot: "Robot",
-            Cronenberg: "Cronenberg",
-            Disease: "Maladie"
-          }
-        },
-        de: {
-          datagrid: {
-            allCharacters_btn: "Alle Charaktere",
-            favorites_btn: "Favoriten",
-            photo_th: "Foto",
-            characterId_th: "Charakter-ID",
-            name_th: "Name",
-            gender_th: "Geschlecht",
-            species_th: "Spezies",
-            lastEpisode_th: "Letzte Folge",
-            addToFavorites_th: "Zu den Favoriten",
-          },
-          gender: {
-            Male: "Männlich",
-            Female: "Weiblich",
-            Genderless: "Geschlechtslos",
-            unknown: "Unbekannt"
-          },
-          species: {
-            Human: "Mensch",
-            Alien: "Außerirdischer",
-            unknown: "Unbekannt",
-            Humanoid: "Humanoid",
-            Poopybutthole: "Poopybutthole",
-            Animal: "Tier",
-            MythologicalCreature: "Mythologische Kreatur",
-            Robot: "Roboter",
-            Cronenberg: "Cronenberg",
-            Disease: "Krankheit"
+      })
+      const state = reactive({
+        searchType: SearchBy.Name,
+        searchText: '',
+        activeTab: TabType.AllCharacters,
+        pages: 0,
+        characters: [] as Character[],
+        favorites: [] as Character[],
+      })
+
+      onMounted(() => {
+        readFavorites()
+        navigateTo(1)
+      })
+
+      const allCharactersTabSelected = computed(
+        () => state.activeTab === TabType.AllCharacters
+      )
+      const favoritesTabSelected = computed(
+        () => state.activeTab === TabType.Favorites
+      )
+      const onTabClicked = (tabType: TabType) => (state.activeTab = tabType)
+
+      const navigateTo = async (page: number) => {
+        const response = await getCharacters(page)
+        state.pages = response.info.pages
+        state.characters = response.results
+      }
+
+      const toggleFavorite = (character: Character) => {
+        if (isFavorite(character)) {
+          state.favorites = state.favorites.filter((c) => c.id !== character.id)
+        } else {
+          state.favorites.push(character)
+        }
+        nextTick(() => saveFavorites())
+      }
+
+      const isFavorite = (character: Character) =>
+        state.favorites.map((c) => c.id).includes(character.id)
+
+      watch(
+        () => favoritesTabSelected.value,
+        async (newValue) => {
+          if (favoritesTabSelected.value) {
+            try {
+              state.favorites = await fetchByIds(
+                state.favorites.map((c) => c.id)
+              )
+            } catch (e) {
+              console.warn(e)
+            }
           }
         }
-      }
-    })
-    const state = reactive({
-      searchType: SearchBy.Name,
-      searchText: "",
-      activeTab: TabType.AllCharacters,
-      pages: 0,
-      characters: [] as Character[],
-      favorites: [] as Character[]
-    });
+      )
 
-    onMounted(() => {
-      readFavorites();
-      navigateTo(1);
-    })
-
-    const allCharactersTabSelected = computed(() => state.activeTab === TabType.AllCharacters)
-    const favoritesTabSelected = computed(() => state.activeTab === TabType.Favorites)
-    const onTabClicked = (tabType: TabType) => state.activeTab = tabType;
-
-    const navigateTo = async (page: number) => {
-      const response = await getCharacters(page);
-      state.pages = response.info.pages;
-      state.characters = response.results;
-    }
-
-    const toggleFavorite = (character: Character) => {
-      if (isFavorite(character)) {
-        state.favorites = state.favorites.filter(c => c.id !== character.id);
-      } else {
-        state.favorites.push(character);
-      }
-      nextTick(() => saveFavorites());
-    }
-
-    const isFavorite = (character: Character) => state.favorites
-        .map(c => c.id)
-        .includes(character.id);
-
-    watch(() => favoritesTabSelected.value, async (newValue) => {
-      if (favoritesTabSelected.value) {
-        try {
-          state.favorites = await fetchByIds(state.favorites.map(c => c.id));
-        } catch (e) {
-          console.warn(e);
+      const saveFavorites = () =>
+        localStorage.setItem('favorites', JSON.stringify(state.favorites))
+      const readFavorites = () => {
+        let item = localStorage.getItem('favorites')
+        if (item) {
+          state.favorites = JSON.parse(item)
+        } else {
+          state.favorites = []
         }
       }
-    });
 
-    const saveFavorites = () => localStorage.setItem("favorites", JSON.stringify(state.favorites));
-    const readFavorites = () => {
-      let item = localStorage.getItem("favorites");
-      if (item) {
-        state.favorites = JSON.parse(item);
-      } else {
-        state.favorites = [];
-      }
-    }
-
-    const characters = computed(() => {
-      if (allCharactersTabSelected.value) {
-        return state.characters;
-      } else if (favoritesTabSelected.value) {
-        const favoritesClone = [...state.favorites];
-        if (state.searchType === SearchBy.Name) {
-          return favoritesClone.filter(c => c.name.toLowerCase().includes(state.searchText.toLowerCase()));
-        } else if (state.searchType === SearchBy.Identifier) {
-          return favoritesClone.filter(c => {
-            const idAsString = c.id + "";
-            return idAsString.includes(state.searchText);
-          });
-        } else if (state.searchType === SearchBy.Episode) {
-          return favoritesClone.filter(c => lastEpisodeOf(c).toLowerCase().includes(state.searchText.toLowerCase()));
+      const characters = computed(() => {
+        if (allCharactersTabSelected.value) {
+          return state.characters
+        } else if (favoritesTabSelected.value) {
+          const favoritesClone = [...state.favorites]
+          if (state.searchType === SearchBy.Name) {
+            return favoritesClone.filter((c) =>
+              c.name.toLowerCase().includes(state.searchText.toLowerCase())
+            )
+          } else if (state.searchType === SearchBy.Identifier) {
+            return favoritesClone.filter((c) => {
+              const idAsString = c.id + ''
+              return idAsString.includes(state.searchText)
+            })
+          } else if (state.searchType === SearchBy.Episode) {
+            return favoritesClone.filter((c) =>
+              lastEpisodeOf(c)
+                .toLowerCase()
+                .includes(state.searchText.toLowerCase())
+            )
+          }
+          return favoritesClone
         }
-        return favoritesClone;
+        return []
+      })
+
+      const lastEpisodeOf = (character: Character): string =>
+        character.episode[character.episode.length - 1].episode
+
+      return {
+        t: i18n.t,
+        state,
+        allCharactersTabSelected,
+        favoritesTabSelected,
+        onTabClicked,
+        TabType,
+        navigateTo,
+        toggleFavorite,
+        isFavorite,
+        characters,
+        lastEpisodeOf,
       }
-      return [];
-    });
-
-    const lastEpisodeOf = (character: Character): string => character.episode[character.episode.length - 1].episode;
-
-    return {
-      t: i18n.t,
-      state,
-      allCharactersTabSelected,
-      favoritesTabSelected,
-      onTabClicked,
-      TabType,
-      navigateTo,
-      toggleFavorite,
-      isFavorite,
-      characters,
-      lastEpisodeOf
-    }
-  }
-})
+    },
+  })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 
-.tab {
-  @apply border-b-2 border-gray-500 cursor-pointer;
-}
+  .tab {
+    @apply border-gray-500 cursor-pointer;
+  }
 
-.selectedTab {
-  @apply border-colliersCyan-400 text-colliersCyan-400;
-}
+  .selectedTab {
+    @apply border-b-4 border-colliersCyan-400 text-colliersCyan-400;
+  }
 
-.isFavorite {
-  @apply bg-colliersCyan-400 text-white;
-}
+  .isFavorite {
+    @apply bg-colliersCyan-400 text-white;
+  }
 
-.isNotFavorite {
-  @apply bg-white text-colliersCyan-400;
-}
-
+  .isNotFavorite {
+    @apply bg-white text-colliersCyan-400;
+  }
 </style>

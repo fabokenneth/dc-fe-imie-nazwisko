6869410f46dc88c0218f5b9f3ed21e85939095cd
Kenneth Fabo <fabokenneth@gmail.com>
2021-04-26 03:49:08 +0100

    FEATURE: Poppins font now used

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,29 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  //mode: "jit",
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        colliersCyan: {
-          400: '#11B0C8',
+    //mode: "jit",
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                colliersCyan: {
+                    400: '#11B0C8',
+                },
+                colliersGray: {
+                    300: '#E5EAF4',
+                    400: '#A9B1BD',
+                }
+            },
+            fontFamily: {
+                'poppins': ['Poppins'],
+            }
         },
-        colliersGray: {
-          300: '#E5EAF4',
-          400: '#A9B1BD',
+    },
+    variants: {
+        extend: {
+            borderWidth: ['hover'],
         },
-      },
     },
-  },
-  variants: {
-    extend: {
-      borderWidth: ['hover'],
-    },
-  },
-  plugins: [require('@tailwindcss/aspect-ratio')],
+    plugins: [require('@tailwindcss/aspect-ratio')],
 }
